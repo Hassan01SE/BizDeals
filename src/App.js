826cb1d6a4775af2cabd4cdae96830efd10aef40
Hassan01SE <hassansohail.se2001@gmail.com>
2023-05-18 02:26:14 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import LandingPage from './components/pages/Landing';
+import NavHead from './components/pages/NavHead';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+
+    document.title = "BizDeals";
+
+  }, [])
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-content">
+        <NavHead />
+        <LandingPage />
+      </div>
     </div>
   );
 }
---
+++ src/components/pages/NavHead.js
@@ -0,0 +1,49 @@
+import '../style/Navhead.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+
+const NavHead = () => {
+    return (
+
+        <Navbar expand="lg">
+            <Container>
+                <Navbar.Brand className='logo' href="#home">BizDeals</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <NavDropdown title="Businesses" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">All Businesses</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.1">Restaurants</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Ecommerce
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Softwares/Websites</NavDropdown.Item>
+
+                        </NavDropdown>
+                        <Nav.Link href="#link">List a Business</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+        /*  <Navbar >
+             <Container>
+                 <Navbar.Brand className='logo' href="#home">BizDeals</Navbar.Brand>
+                 <Nav className="me-auto">
+                     <Nav.Link href="#home">Home</Nav.Link>
+                     <Nav.Link href="#">Businesses</Nav.Link>
+                     <Nav.Link href="#">List a Business</Nav.Link>
+                 </Nav>
+             </Container>
+         </Navbar> */
+
+    );
+}
+
+export default NavHead;
